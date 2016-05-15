080df033beef84006f8ed8f9d353569d61a5e742
Edouard Long <edouardlong@icloud.com>
2016-05-15 13:05:13 +0100

    Putting subs into the formation and figuring out which row the user has chosen

--- README.md
+++ README.md
@@ -1,64 +1,9 @@
-# Football-Draft
-The repository
-def showSquad(formation, players):
+This is the footbal draft repository 
 
-    # first clear the screen
-    print("\n" * 80)
+Members:
 
-    # print a title in the middle of the screen
-    print("                                                                        YOUR SQUAD")
-    print("\n" * 5)
-
-    # initialise a counter that keeps track of which player we are on
-    counter = 0 
-
-    # initialise a loop that will loop through each row of the formation and print out the rows inside it
-    for row in formation:
-
-        # print out 10 new lines ( dont if this is the fist row)
-        if counter != 0:
-            print("\n" * 10)
-
-        # initialse a new string that contains the whole of each row
-        contentsOfRow = ""
-
-        # if there are only 1,2 or 3 players, add some white space to make the list appear in the middle
-        if row == 1:
-            contentsOfRow += "                                                              "
-        elif row == 2:
-            contentsOfRow += "                                         "
-        elif row == 3:
-            contentsOfRow += "                               "
-        elif row == 4:
-            contentsOfRow += "               "
-
-        # initialise a loop that goes through the len of each row
-        for player in range(0, row):
-
-            # add some white space to the string
-            contentsOfRow = contentsOfRow + "      "
-
-            # add the player's name to the string
-            contentsOfRow += players[counter][0].capitalize() + "    "
-
-            # add the players position to the string
-            contentsOfRow += players[counter][1].upper()+ "    "
-
-            # add the player's rating to the string
-            contentsOfRow += players[counter][2]+ "    "
-
-            #move onto the next player
-            counter += 1
-
-        # print out the row
-        print(contentsOfRow)
-
-
-# create an array of 11 players
-players = [["Player 1","striker","87"],["Player 2","striker","87"],["Player 3","mid","87"],["Player 4","mid","87"],["Player 5","mid","87"],["Player 6","mid","87"],["Player 7","mid","87"],["Player 8","mid","87"],["Player 9","def","87"],["Player 10","def","87"],["Player 11","gk","87"]]
-
-# create a formation we will use
-formation = [2,5,3,1]
-
-# print out the squad
-showSquad(formation,players)
+Rupert Walker
+Stan Turner
+Zain Mahmood
+Archie Polglase
+Edouard Long
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # Edouard Long 2015
 
 # a function that will display the formation with all of the given players ( takes in a formation and the players )
-def showSquad(formation, players):
+def showSquad(formation,players,subs):
 
     # first clear the screen
     print("\n" * 80)
@@ -17,7 +17,7 @@ def showSquad(formation, players):
 
         # print out 10 new lines ( dont if this is the fist row)
         if counter != 0:
-            print("\n" * 10)
+            print("\n" * 5)
 
         # initialse a new string that contains the whole of each row
         contentsOfRow = ""
@@ -53,13 +53,96 @@ def showSquad(formation, players):
         # print out the row
         print(contentsOfRow)
 
+    # print out the subs
+    print("\n\n\n\n\n\n\nSubs:\n")
 
-# create an array of 11 players
-players = [["Player 1","striker","87"],["Player 2","striker","87"],["Player 3","mid","87"],["Player 4","mid","87"],["Player 5","mid","87"],["Player 6","mid","87"],["Player 7","mid","87"],["Player 8","mid","87"],["Player 9","def","87"],["Player 10","def","87"],["Player 11","gk","87"]]
+    # initialise the row that contians the subs
+    contentsOfRowSubs = ""
 
-# create a formation we will use
-formation = [3,3,4,1]
+    # initialse a counter
+    counter = 0
 
-# print out the squad
-showSquad(formation,players)
+    # initialse a new string that contains the whole of each row
+    contentsOfRowSubs = ""
+
+    # loop through all the subs and print them out
+    for sub in subs:
+
+        # add some white space to the string except if it is first sub
+        if counter != 0 :
+            contentsOfRowSubs = contentsOfRowSubs + "    "
+
+        # add the subs's name to the string
+        contentsOfRowSubs += subs[counter][0].capitalize() + "    "
+
+        # add the subs position to the string
+        contentsOfRowSubs += subs[counter][1].upper()+ "    "
+
+        # add the subs's rating to the string
+        contentsOfRowSubs += subs[counter][2]+ "    "
+
+        #move onto the next player
+        counter += 1
+
+    # print the subs
+    print(contentsOfRowSubs)
+
+        
+    
+# create a main func
+def main():
+
+    # create an array of 11 players
+    players = [["Player 1","striker","87"],["Player 2","striker","87"],["Player 3","mid","87"],["Player 4","mid","87"],["Player 5","mid","87"],["Player 6","mid","87"],["Player 7","mid","87"],["Player 8","mid","87"],["Player 9","def","87"],["Player 10","def","87"],["Player 11","gk","87"]]
+
+    # create an array of 5 subs
+    subs = [["Player 12","striker","87"],["Player 13","DEF","87"],["Player 14","GK","87"],["Player 15","LW","87"],["Player 16","RW","87"]]
+
+    # create a formation we will use
+    formation = [3,3,4,1]
+
+    # print out the squad
+    showSquad(formation,players,subs)
+
+    print("\n\n")
+    
+    # get the user to choose a number
+    userInput = int(input("Please choose a player to swap (type in their number)\n"))
+    
+    # check if the user has chosen a sub
+    if userInput > 11:
+        userRow = len(formation) + 1
+
+    # else find the row of the player they chose
+    else:
+        # initialise a currentrow value
+        currentRow = 0
+
+        # initialise the users row
+        userRow = 0
+
+        # determine which row the user's player is on
+        for row in range(0,len(formation)):
+
+            # get the number of players in the next row
+            currentRow = currentRow + formation[row]
+                    
+            # check if the player is in this row
+            if (currentRow - userInput) >= 0:
+
+                # the user has chosen this row
+                userRow = row + 1
+                
+                break
+
+    print("The row you have chosen is " + str(userRow))
+
+
+main()
+
+        
+
+    
+
+    
     
