13f913b2d0929ac0c814c94de064333125531348
stanleyturner <constantine.turner@gmail.com>
2016-05-16 09:43:07 +0100

    FORMATIONS - STAN

---
+++ formations.py
@@ -0,0 +1,27 @@
+import time
+import random
+
+def pickFormation():
+    formation = []
+    formation.append() = [['gk'],['cb','cb','cb',],['cm','cm',],['lm','rm'],['cam']['st','st']]
+    formation.append() = [['gk'],['cb','cb','cb'],['lm','cm','cm','rm'],['lf','rf'],['st']]
+    formation.append() = [['gk'],['cb','cb','cb'],['lm','cm','cm','rm'],[['lf','st','rf']]
+    formation.append() = [['gk'],['cb','cb','cb'],['cdm','cdm'],['lm','cam','rm'],['st','st']
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cdm'],['lm','rm'],['cam'],['st','st']]
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cdm'],['lm','cm','cm','rm'],['st']]
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cdm','cdm'],['cam']['cam','cam'],['st']]
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cdm','cdm',],['cam','cam'],['st','st']]
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cdm'],['lm','cm','cm','rm'],['st']]
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cm','cm','cm'],['cam'],['st','st']]
+    formation.append() = [['gk'],['lb','cb','cb','rb'],['cm','cm','cm',],['lf','rf'],['st']]
+    
+    
+    
+    
+    [3412, 3421, 343, 352, 41212, 4141, 4231, 4222, 4312, 4321, 433, 4411, 442, 451, 5212, 5221, 532]
+    
+    
+    
+    
+    
+pickFormation()   
